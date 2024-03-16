f75cf0314871e19208a64f36c3392ca6d6cc82fa
DmitriyVolchenko <Dmitriy.Volchenko@gmail.com>
2024-03-16 09:45:25 +0200

    adds mobileIsraelPhoneTest()

--- src/telran/strings/test/RegularExpresionsTests.java
+++ src/telran/strings/test/RegularExpresionsTests.java
@@ -84,6 +84,71 @@ class RegularExpresionsTests {
 	
 	@Test
 	void mobileIsraelPhoneTest() {
+		String regex = RegularExpresions.mobileIsraelPhone();
+		
+		//checking true
+		assertTrue("+972-50-0000000".matches(regex));
+		assertTrue("+972-55-7654321".matches(regex));		
+		assertTrue("+972-59-9999999".matches(regex));
+		assertTrue("050-0000000".matches(regex));
+		assertTrue("053-1234567".matches(regex));
+		assertTrue("059-9999999".matches(regex));
+		
+		//checking false prefix 
+		assertFalse("972-50-0000000".matches(regex));
+		assertFalse("-972-50-0000000".matches(regex));
+		assertFalse("+9_2-50-0000000".matches(regex));
+		assertFalse("a972-50-0000000".matches(regex));
+		assertFalse("!972-50-0000000".matches(regex));
+		assertFalse("0972-50-0000000".matches(regex));
+		assertFalse("+933-50-0000000".matches(regex));
+		assertFalse("@933-50-0000000".matches(regex));
+		
+		//checking false second prefix 
+		assertFalse("+972-65-7654321".matches(regex));		
+		assertFalse("+972-43-7654321".matches(regex));
+		assertFalse("+972-5a-7654321".matches(regex));
+		assertFalse("+972-!3-7654321".matches(regex));
+		assertFalse("+972-5@-7654321".matches(regex));
+		assertFalse("+972-533-7654321".matches(regex));
+		assertFalse("153-1234567".matches(regex));
+		assertFalse("1530-1234567".matches(regex));
+		assertFalse("+53-1234567".matches(regex));
+		assertFalse("063-1234567".matches(regex));
+		assertFalse("0_3-1234567".matches(regex));
+		assertFalse("05a-1234567".matches(regex));
+		assertFalse("03-1234567".matches(regex));
+		assertFalse("@3-1234567".matches(regex));
+		
+		//checking false postfix 
+		
+		assertFalse("+972-59-99-9999".matches(regex));
+		assertFalse("+972-59-99999999".matches(regex));
+		assertFalse("+972-59-999999".matches(regex));
+		assertFalse("+972-59-999999!".matches(regex));
+		assertFalse("+972-59-9999z99".matches(regex));
+		assertFalse("+972-59-!000000".matches(regex));
+		assertFalse("+972-59-0@00000".matches(regex));
+		assertFalse("053-12345678".matches(regex));
+		assertFalse("053-123456".matches(regex));
+		assertFalse("053-123456!".matches(regex));
+		assertFalse("053-1234z67".matches(regex));
+		assertFalse("053-!234567".matches(regex));
+		assertFalse("055-1@34567".matches(regex));
+		
+		//checking false hyphens 
+		
+		assertFalse("+972599999999".matches(regex));
+		assertFalse("+97-2599999999".matches(regex));
+		assertFalse("+9725-99999999".matches(regex));
+		assertFalse("+972599999-999".matches(regex));
+		assertFalse("+972_59_9999999".matches(regex));
+		assertFalse("+972-59-999-99-99".matches(regex));
+		assertFalse("0599999999".matches(regex));
+		assertFalse("05-99999999".matches(regex));
+		assertFalse("059-999-99-99".matches(regex));
+		
+		
 	}
 	}
 	
